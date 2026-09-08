cfc17fd0b7fa31d6c7d59656e9c50bb9ecda5800
balajiyedida5929 <vtu28802@veltech.edu.in>
2026-09-08 09:17:04 +0530

    Update Real-Time Stream Analytics Engine.java

--- Task-2/Real-Time Stream Analytics Engine.java
+++ Task-2/Real-Time Stream Analytics Engine.java
@@ -18,22 +18,22 @@ public class Main {
         }
 
         readings.stream()
-                // 1. Filter temperatures greater than 50
+            
                 .filter(r -> r.temperature > 50)
 
-                // 2. Group by Sensor ID
+
                 .collect(Collectors.groupingBy(
                         r -> r.sensorId,
                         Collectors.averagingDouble(r -> r.temperature)
                 ))
 
-                // 3. Convert to stream of Map entries
+           
                 .entrySet().stream()
 
-                // 4. Sort by average temperature descending
+        
                 .sorted((a, b) -> Double.compare(b.getValue(), a.getValue()))
 
-                // Display result
+           
                 .forEach(e -> System.out.println(
                         e.getKey() + " " + e.getValue()
                 ));
@@ -51,3 +51,13 @@ public class Main {
         }
     }
 }
+Input : 6 
+    S1 60 
+    S2 40
+    S1 80 
+    S3 70 
+    S2 90
+    S3 30 
+Output :  S1 70.0 
+          S2 90.0 
+          S3 70.0 
